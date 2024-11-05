0bf91853fa4b816716f2a68811cbe1d99b3d07b6
LazerSharp <email2barun@gmail.com>
2024-11-05 22:58:41 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
+void removeNewLine(char *str) {
+    str[strcspn(str, "\n")] = 0;
+}
+
+int execCmd(char *cmd) {
+    removeNewLine(cmd);
+    if(strcmp("exit 0", cmd) == 0) {
+      return -1;
+    } 
+    printf("%s: command not found\n", cmd);
+    return 0;
+}
+
 int main() {
   char input[100];
   for(;;) {
     // Uncomment this block to pass the first stage
     printf("$ ");
     fflush(stdout);
-
     // Wait for user input
     fgets(input, 100, stdin);
-    input[strcspn(input, "\n")] = 0;
-    printf("%s: command not found\n", input);
+    int ret = execCmd(input);
+    if(ret == -1) {
+      return 0;
+    }
   }
   return 0;
 }
+
