1018e2e07589b24ba9dc3a727a90b20c4df37f06
LazerSharp <email2barun@gmail.com>
2024-11-05 22:40:56 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -2,14 +2,16 @@
 #include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
-
-  // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = 0;
-  printf("%s: command not found\n", input);
+  for(;;) {
+    // Uncomment this block to pass the first stage
+    printf("$ ");
+    fflush(stdout);
+
+    // Wait for user input
+    fgets(input, 100, stdin);
+    input[strcspn(input, "\n")] = 0;
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
