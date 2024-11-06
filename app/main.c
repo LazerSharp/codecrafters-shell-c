66e5375b80f9cb2249a08ab37aa24f2af4c17966
LazerSharp <email2barun@gmail.com>
2024-11-07 02:51:50 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 void removeNewLine(char *str) {
     str[strcspn(str, "\n")] = 0;
@@ -9,15 +12,37 @@ int startsWith(char *pre, char *str) {
   return strncmp(pre, str, strlen(pre)) == 0;
 }
 
+char* searchExternalCmd(char *cmd) {
+  char* paths = getenv("PATH");
+  char* path;
+  while((path = strsep(&paths, ":")) != NULL) {
+    char* p = strdup(path);
+    p = strcat(p, "/");
+    p = strcat(p, cmd);
+    struct stat buf;
+    if(stat(p, &buf) == 0 && (buf.st_mode & X_OK)) {
+      return p;
+    }
+  }
+  return NULL;
+}
+
 char *builtins[] = {"type", "echo", "exit"}; 
 void type(char *cmd) {
   int len = sizeof(builtins) / sizeof(builtins[0]);
+  // search builtins
   for (int i=0; i < len; i++) {
     if(strcmp(builtins[i], cmd) == 0) {
       printf("%s is a shell builtin\n", cmd);
       return;
     }
-  }  
+  }
+  // search extternal commads in $PATH
+  char* loc;
+  if((loc = searchExternalCmd(cmd)) != NULL) {
+    printf("%s is %s\n",cmd, loc);
+    return;
+  }
   printf("%s: not found\n", cmd);
 }
 
