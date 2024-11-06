1db12f05501caf61132b66b32f09b93843d982cf
LazerSharp <email2barun@gmail.com>
2024-11-07 01:00:52 +0530

    codecrafters submit [skip ci]

--- app/main.c
+++ app/main.c
@@ -5,11 +5,19 @@ void removeNewLine(char *str) {
     str[strcspn(str, "\n")] = 0;
 }
 
+int startsWith(char *pre, char *str) {
+  return strncmp(pre, str, strlen(pre)) == 0;
+}
+
 int execCmd(char *cmd) {
     removeNewLine(cmd);
     if(strcmp("exit 0", cmd) == 0) {
       return -1;
-    } 
+    }
+    if(startsWith("echo ", cmd)) {
+      puts(&cmd[5]);
+      return 0;
+    }
     printf("%s: command not found\n", cmd);
     return 0;
 }
